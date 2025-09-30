baf10aa492a392ae2cfadadf433f5ff1aa0f27db
yeshwanthkumars <yeshwanthkumarsrinivasan@gmail.com>
2025-09-30 22:54:12 +0530

    spring-boot-kafka-user-service added

---
+++ spring-boot-kafka-user-service/src/main/java/com/example/demo/SpringBootKafkaUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaUserServiceApplication.class, args);
+	}
+
+}
